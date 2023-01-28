927467b6cab967dcca3416c4d99a405f0115c8ed
riandroraul <riandroraul@gmail.com>
2023-01-28 14:44:13 +0700

    update UI

--- mern-blog-frontend/src/config/Router/index.js
+++ mern-blog-frontend/src/config/Router/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home, Login, Register } from "../../pages";
+import { Home, Login, MainApp, Register } from "../../pages";
 
 const AllRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MainApp />}>
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
--- mern-blog-frontend/src/pages/Login/index.js
+++ mern-blog-frontend/src/pages/Login/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { loginImage } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="main-page">
+      <div className="left">
+        <img src={loginImage} alt="images" className="bg-image" />
+      </div>
+      <div className="right">
+        <p className="title" s>
+          Form Login
+        </p>
+        <Input label="Email" placeholder="Email" />
+        <Gap height={8} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Login" />
+        <Gap height={100} />
+        <Link title="belum punya akun, silahkan daftar" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
