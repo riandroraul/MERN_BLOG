ed4cd9494f06cdce82ecf661283919267b398c53
riandroraul <riandroraul@gmail.com>
2023-01-28 13:25:38 +0700

    update UI

--- mern-blog-frontend/src/pages/Register/index.js
+++ mern-blog-frontend/src/pages/Register/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./register.scss";
+import { registerImage } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
 
 const Register = () => {
-  return <div>Register Page</div>;
+  return (
+    <div className="main-page">
+      <div className="left">
+        <img src={registerImage} alt="images" className="bg-image" />
+      </div>
+      <div className="right">
+        <p className="title" s>
+          Form Register
+        </p>
+        <Input label="Fullname" placeholder="Full Name" />
+        <Gap height={8} />
+        <Input label="Email" placeholder="Email" />
+        <Gap height={8} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Register" />
+        <Gap height={100} />
+        <Link title="Kembali ke login" />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
